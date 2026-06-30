0c5274a376aa72e4e6e55d3cdcb1dd20595dbd19
vidhaanwastaken <vidhaantripathi@gmail.com>
2026-06-30 11:33:42 +0530

    implemented redis into all the methods

---
+++ config/redis.js
@@ -0,0 +1,16 @@
+const { createClient } = require('redis');
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+});
+
+redisClient.on('error', (err) => {
+    console.log('Redis Client Error', err);
+});
+
+(async () => {
+    await redisClient.connect();
+    console.log('Redis connected successfully');
+})();
+
+module.exports = redisClient;
--- controller/projectController.js
+++ controller/projectController.js
@@ -2,20 +2,25 @@
 
 const { where } = require('sequelize');
 const db = require('../db/models');
-
+const redisClient = require('../config/redis');
 const project = db.project;
 const users = db.users;
-
+const getCache = require('../utils/rediscache').getCache;
+const setCache = require('../utils/rediscache').setCache;
+const deleteCache = require('../utils/rediscache').deleteCache;
+// const projectId = require('../db/models/project').project.id;
 
 // const project = require('../db/models/project');
 // const users = require('../db/models/user');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const useRedisCache = require('../utils/rediscache');
+
 
 const createProject = catchAsync(async (req, res, next) => {
     const body = req.body;
     const userId = req.users.id;
-    // const userId = req.user.id;
+    
     const newProject = await project.create({
         title: body.title,
         productImage: body.productImage,
@@ -28,6 +33,10 @@ const createProject = catchAsync(async (req, res, next) => {
         createdBy: userId,
     });
 
+const key = await `project:${newProject.id}`
+await setCache(key, newProject);
+
+
 return res.status(201).json({
     status: 'success',
     data: newProject
@@ -35,6 +44,7 @@ return res.status(201).json({
 })
 
 const getAllProjects = catchAsync(async (req, res, next) => {
+
     const userId = req.users.id;
     const result = await project.findAll({
         include: users,
@@ -49,17 +59,46 @@ const getAllProjects = catchAsync(async (req, res, next) => {
 })
 
 const getProjectById = catchAsync(async (req, res, next) => {
+
     const projectId = req.params.id;
-    const result = await project.findByPk(projectId, {include: users});
-    if(!result) {
-        return next(new AppError('invalid project id', 400))
+    const key = `project:${projectId}`;
+
+    
+    const cachedProject = await getCache(key);
+
+    if (cachedProject) {
+        console.log("Data fetched from Redis");
+
+        return res.status(200).json({
+            status: 'success',
+            source: 'got data from redis cache',
+            data: cachedProject
+        });
     }
+
+
+    const projectData = await project.findOne({
+        where: { id: projectId }
+    });
+
+    
+    if (!projectData) {
+        return next(new AppError('Invalid project ID', 404));
+    }
+
+    
+    await setCache(key, projectData, 5);
+
+    console.log("Data fetched from PostgreSQL and cached");
+
     
     return res.status(200).json({
-        status:'sucess',
-        data: result
-    })
-})
+        status: 'success',
+        source: 'got data from postgres database',
+        data: projectData
+    });
+
+});
 
 const updateProject = catchAsync(async (req, res, next) => {
     const userId = req.users.id;
@@ -81,8 +120,11 @@ const updateProject = catchAsync(async (req, res, next) => {
     result.category = body.category
     result.tags = body.tags
     
-
+   
     const updatedResult = await result.save();
+
+    const key = await `project:${updatedResult.id}`
+    await setCache(key, updatedResult, 30);
 
     return res.status(200).json({
         status:'success',
@@ -113,6 +155,9 @@ const deleteProject = catchAsync(async (req, res, next) => {
   force: true,
 });
  
+    const key = await `project:${projectId}`
+    await deleteCache(key);
+
 
     return res.json({
         status: 'success',
@@ -127,6 +172,7 @@ const deleteProject = catchAsync(async (req, res, next) => {
     //     data: result
     // })
 })
-   
+
+
 module.exports = {createProject, getAllProjects, getProjectById, updateProject,deleteProject}
 
